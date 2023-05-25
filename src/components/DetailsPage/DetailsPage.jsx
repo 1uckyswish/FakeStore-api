3e5a43ee8804c06e79ba8fbc6ce1bb5f42200c01
Noel Guillen <noelblas274@gmail.com>
2023-05-25 15:15:28 -0400

    finished routing JSON data to card with params ID

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 import "./DetailsPage.css"
+import {useParams} from 'react-router-dom'
+import axios from 'axios';
 
 function DetailsPage() {
+  //* returns the details for the specific product when button/tab is clicked
+  //* return the id from the url for the details to render.
+  const {productId} = useParams();
+  //* call the api to load the specific product on page
+  //* https://fakestoreapi.com/products/
+
+  //* store the current JSON data in state to apply to XML
+  const [itemDetails, setItemDetails] = useState([]);
+
+  useEffect(
+    ()=>{
+      //*fetch data fron the api and apply the current ID of the param to the API URL to access the JSON data
+     axios.get(`https://fakestoreapi.com/products/${productId}`)
+     .then((result)=>
+     {
+      console.log(result.data)
+      //*Call state function to store the JSON data */
+      setItemDetails(result.data)
+     }
+     )
+     .catch((error)=>console.log(error))
+    }, [] //* when page loads it runs
+  )
+
   return (
     <div className='details-main-container'>
         <div className='details-container'>
             <div className='details-img'>
-                <img src='https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg'/>
+                <img src={itemDetails.image}/>
             </div>
             <div className='details-info'>
-                <h3>Fjallraven Backpack</h3>
-                <p>109€</p>
+                <h3>{itemDetails.title}</h3>
+                <p>${itemDetails.price}</p>
                 <p>Description</p>
-                <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                <p>{itemDetails.description}</p>
                 <button>Add to Cart</button>
             </div>
         </div>

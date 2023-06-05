fc991a589b61c0185f13d50ae69defad86adc86f
Noel Guillen <noelblas274@gmail.com>
2023-06-04 21:05:11 -0400

    contact us page dark mode added

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import {useContext} from 'react'
 import "./Contact.css"
+import { ThemeContext } from '../../contexts/DarkModeContext'
 
 function ContactUs() {
   //* a box with 3 inputs for user to send their data to server
+    //** toggle dark mode */
+  //** use context for global state */
+  const {darkMode} = useContext(ThemeContext);
   return (
-    <div className='contact-container'>
-        <div className='form-container'>
+    <div className={
+      darkMode?
+      'contact-container'
+      :
+      'contact-container dark-contact-container'
+    }>
+        <div className={
+          darkMode?
+          'form-container'
+          :
+          'form-container dark-form-container'
+        }>
             <h1>Contact Us</h1>
             <input type="text" placeholder='First Name'/>
             <input type="text" placeholder='Last Name'/>
